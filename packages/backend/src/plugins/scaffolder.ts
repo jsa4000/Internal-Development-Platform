e522b628e4b46d2458a5e49bf90964ed66cb83ef
Javier Santos Andrés <jsa4000@gmail.com>
2022-03-13 23:12:25 +0100

    Added yeoman plugin, templates and default configuration

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,5 +1,7 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createRouter, createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
+import { createRunYeomanAction } from '@backstage/plugin-scaffolder-backend-module-yeoman';
+import { ScmIntegrations } from '@backstage/integration';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
 
@@ -11,11 +13,22 @@ export default async function createPlugin({
   discovery,
 }: PluginEnvironment): Promise<Router> {
   const catalogClient = new CatalogClient({ discoveryApi: discovery });
+  const integrations = ScmIntegrations.fromConfig(config);
+  const actions = [
+    createRunYeomanAction(),
+    ...createBuiltinActions({
+      integrations,
+      config,
+      catalogClient,
+      reader,
+    }),
+  ];
   return await createRouter({
     logger,
     config,
     database,
     catalogClient,
     reader,
+    actions,
   });
 }
