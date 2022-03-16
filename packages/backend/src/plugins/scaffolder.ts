1be2b6016d76ae8526bf3082c4e25823ef7c91cf
Javier Santos Andrés <jsa4000@gmail.com>
2022-03-16 23:28:42 +0100

    Added scaffolder modules utils and http request

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,6 +1,8 @@
 import { CatalogClient } from '@backstage/catalog-client';
 import { createRouter, createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
 import { createRunYeomanAction } from '@backstage/plugin-scaffolder-backend-module-yeoman';
+import { createZipAction, createWriteFileAction, createAppendFileAction, createSleepAction } from '@roadiehq/scaffolder-backend-module-utils';
+import { createHttpBackstageAction } from '@roadiehq/scaffolder-backend-module-http-request';
 import { ScmIntegrations } from '@backstage/integration';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
@@ -16,12 +18,17 @@ export default async function createPlugin({
   const integrations = ScmIntegrations.fromConfig(config);
   const actions = [
     createRunYeomanAction(),
+    createWriteFileAction(),
+    createSleepAction(),
+    createZipAction(),
+    createAppendFileAction(),
+    createHttpBackstageAction({ config }),
     ...createBuiltinActions({
-      integrations,
-      config,
-      catalogClient,
-      reader,
-    }),
+    integrations,
+    config,
+    catalogClient,
+    reader,
+  }),
   ];
   return await createRouter({
     logger,
